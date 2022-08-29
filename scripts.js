5bc4538d7ce54c5e69cd2c50c7edff03b3deeaf4
Brooks Susor <susorbrooks@gmail.com>
2022-08-29 10:26:11 -0400

    initial commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+let divLibrary = document.querySelector('.divLibrary'); 
+
+let myLibrary = ['Moby Dick', 'The Great Gatsby', '1984']; 
+
+function Book(title, author, pages, read) {
+  this.title = title; 
+  this.author = author; 
+  this.pages = pages; 
+  this.read = read; 
+}
+
+function addBookToLibrary (newBook) {
+  if (!myLibrary.includes(newBook)) {
+    myLibrary.push(newbook); 
+  }
+}
+
+function DisplayLibrary () {
+  myLibrary.forEach(book => {
+    displayBook = document.createElement('div'); 
+    document.appendChild
+  }); 
+}
