ca7f58b316bde4e26f8389cfc87f258489437dc1
Brooks Susor <susorbrooks@gmail.com>
2022-08-29 12:47:55 -0400

    Create basic library setup

--- scripts.js
+++ scripts.js
@@ -1,7 +1,24 @@
-let divLibrary = document.querySelector('.divLibrary'); 
-const buttonForm = document.querySelector('button'); 
+const divLibrary = document.querySelector('.divLibrary'); 
+const buttonDisplayForm = document.querySelector('.btnDisplayF'); 
+const buttonDisplayLibrary = document.querySelector('.btnDisplayL'); 
+const form = document.querySelector('form'); 
 
-let myLibrary = ['Moby Dick', 'The Great Gatsby', '1984']; 
+let myLibrary = [{
+  title: 'Moby Dick', 
+  author: 'Herman Melville', 
+  pages: '150', 
+  read: 'no'
+}, {
+  title: 'The Great Gatsby', 
+  author: 'F. Scott Fitzgerald', 
+  pages: '200', 
+  read: 'yes'
+}, {
+  title: '1984', 
+  author: 'George Orwell', 
+  pages: '120', 
+  read: 'yes'
+}]; 
 
 function Book(title, author, pages, read) {
   this.title = title; 
@@ -12,19 +29,29 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary (newBook) {
   if (!myLibrary.includes(newBook)) {
-    myLibrary.push(newbook); 
+    myLibrary.push(newBook); 
   }
 }
 
-function DisplayLibrary () {
+buttonDisplayLibrary.addEventListener('click', function DisplayLibrary () {
   myLibrary.forEach(book => {
-    pBook = document.createElement('p'); 
-    pBook.textContent = `${book}`; 
+    //create book card
+    pBook = document.createElement('p');
+    pBook.textContent = `${book.title}`; 
     divLibrary.appendChild(pBook); 
-  }); 
-}
 
-buttonForm.addEventListener('click', function(e) {
-  form = document.createElement('form'); 
-  
+    //create book removal button
+    btnRm = document.createElement('button'); 
+    btnRm.textContent = 'Remove Book'; 
+    btnRm.classList.add('btnRm'); 
+    divLibrary.appendChild(btnRm); 
+  }); 
+})
+
+buttonDisplayForm.addEventListener('click', function(e) {
+  if (form.style.display === 'flex') {
+    form.style.display = 'none'; 
+  } else {
+    form.style.display = 'flex'; 
+  }
 })
